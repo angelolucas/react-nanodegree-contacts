2ee74d50ca02ff07d3aed78ead8029da33569827
Ângelo Lucas <angelo.lrc@gmail.com>
2018-05-30 19:43:04 -0400

    list contacts

---
+++ src/ListContacts.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ListContacts = (props) => (
+  <ol className="contact-list">
+    {props.contacts.map((contact, key) => (
+      <li className="contact-list-item" key={contact.id}>
+        <div className="contact-avatar" style={{
+          backgroundImage: `url(${contact.avatarURL})`
+        }} />
+        <div className="contact-details">
+          <p>{contact.name}</p>
+          <p>{contact.email}</p>
+        </div>
+        <button className="contact-remove">
+          Remove
+        </button>
+      </li>
+    ))}
+  </ol>
+);
+
+export default ListContacts
