0d0750bc4fec222e05520025fdc4e455a0dd8bcc
Ângelo Lucas <angelo.lrc@gmail.com>
2018-06-01 17:09:42 -0400

    convert ListContacts to a controlled component with a search field

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const ListContacts = (props) => (
-  <ol className="contact-list">
-    {props.contacts.map((contact, key) => (
-      <li className="contact-list-item" key={contact.id}>
-        <div className="contact-avatar" style={{
-          backgroundImage: `url(${contact.avatarURL})`
-        }} />
-        <div className="contact-details">
-          <p>{contact.name}</p>
-          <p>{contact.email}</p>
+class ListContacts extends Component {
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
+
+  render() {
+    return (
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts"
+            type="text"
+            placeholder="Search contacs"
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+          />
         </div>
-        <button onClick={() => props.onDeleteContact(contact)} className="contact-remove">
-          Remove
-        </button>
-      </li>
-    ))}
-  </ol>
-);
+        <ol className="contact-list">
+          {this.props.contacts.map((contact, key) => (
+            <li className="contact-list-item" key={contact.id}>
+              <div className="contact-avatar" style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }} />
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+              </div>
+              <button onClick={() => this.props.onDeleteContact(contact)} className="contact-remove">
+                Remove
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+    )
+  }
+};
 
 ListContacts.propTypes = {
   contacts: PropTypes.array.isRequired,
